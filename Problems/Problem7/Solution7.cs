e6dbe9cee9f7ded64d3715a194b4ab5197a863df
milosz <miloszloszek@gmail.com>
2018-01-30 19:43:00 +0100

    largest product in a series

---
+++ Problems/Problem7/Solution7.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem7
+{
+    class Solution7
+    {
+        static bool IsPrime(long number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static int FindNthPrime(int n)
+        {
+            var index = 1;
+            var number = 2;
+
+            while (true)
+            {
+                if (IsPrime(number))
+                {
+                    if (index == n)
+                    {
+                        return number;
+                    }
+                    index++;
+                }
+                number++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNthPrime(10001));
+            Console.ReadKey();
+        }
+    }
+}
