4ddc1fdfa562686a7c92a3d59ad493a69e4e236d
milosz <miloszloszek@gmail.com>
2018-01-31 19:43:54 +0100

    sum of primes, folder renamed

---
+++ Problems/Problem10/Solution10.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem10
+{
+    class Solution10
+    {
+        static bool IsPrime(long number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static long FindSumOfPrimes()
+        {
+            long result = 2;
+
+            for (int i = 3; i < 2000000; i += 2)
+            {
+                if (IsPrime(i))
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindSumOfPrimes());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problems/Problem9/Solution9.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9
+{
+    class Solution9
+    {
+        static int a;
+        static int b;
+        static int c;
+
+        static void FindTriple()
+        {
+
+            a = 0;
+            b = 0;
+            c = 0;
+
+            for (int n = 1; n < 40; n++)
+            {
+                for (int m = n + 1; m < 40; m++)
+                {
+                    a = Convert.ToInt32(Math.Pow(m, 2) - Math.Pow(n, 2));
+                    b = 2 * m * n;
+                    c = Convert.ToInt32(Math.Pow(m, 2) + Math.Pow(n, 2));
+                    if (a + b + c == 1000)
+                    {
+                        Console.WriteLine(a * b * c);
+                    }
+
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FindTriple();
+            Console.ReadKey();
+        }
+    }
+}
